f93f5a4c104a3fe5d1b11f6ccf2b79de205a35a9
Sergio Gómez <sergio@uco.es>
2018-07-15 13:19:30 +0200

    Added doctrine user entity

---
+++ src/Infrastructure/Repository/UserViews.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `iam` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AulaSoftwareLibre\Iam\Infrastructure\Repository;
+
+use AulaSoftwareLibre\DDD\Infrastructure\ReadModel\SchemaManagerInterface;
+use AulaSoftwareLibre\Iam\Infrastructure\Entity\UserView;
+
+interface UserViews extends SchemaManagerInterface
+{
+    public function add(UserView $userView): void;
+
+    public function get(string $userId): UserView;
+}
---
+++ src/Infrastructure/Repository/UserViewsRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `iam` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AulaSoftwareLibre\Iam\Infrastructure\Repository;
+
+use AulaSoftwareLibre\DDD\Infrastructure\Doctrine\SchemaManagerORMTrait;
+use AulaSoftwareLibre\Iam\Infrastructure\Entity\UserView;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+class UserViewsRepository extends ServiceEntityRepository implements UserViews
+{
+    use SchemaManagerORMTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserView::class);
+    }
+
+    public function add(UserView $userView): void
+    {
+        $this->_em->persist($userView);
+        $this->_em->flush();
+    }
+
+    public function get(string $userId): UserView
+    {
+        return $this->find($userId);
+    }
+}
